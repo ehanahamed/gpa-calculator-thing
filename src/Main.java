b185c0e4c00e5bfa8a3a4447afec3181ad4d251a
Ehan <ehanahamed@ehan.dev>
2025-09-10 08:10:51 -0400

    asdfsadfasfdasdf

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 public class Main {
     Scanner scanner = new Scanner(System.in);
     System.out.println("GPA Calculator");
-    conti
-    while ()
+    boolean continueLooping = true;
+    while (continueLooping) {
+        String courseName;
+        System.out.print("Course name:");
+
+    }
 }
