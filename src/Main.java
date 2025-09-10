59b461e7a7acd5495aabf34ee50d3858fbed98c3
127VKhan <127vkhan@frhsd.com>
2025-09-10 08:16:50 -0400

    add output/input

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,16 @@ package src;
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
     System.out.println("GPA Calculator");
     boolean continueLooping = true;
     while (continueLooping) {
-        String courseName;
-        System.out.print("Course name:");
+        System.out.print("Course name: ");
+        String courseName = scan.nextLine();
+        System.out.print("Type the course level: ");
+        String courseLevel = scan.nextLine();
+        System.out.print("Enter your grade: ");
+        double courseLevel = scan.nextDouble();
 
     }
 }
