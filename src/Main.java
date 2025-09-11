67c77c97b51b26153d3a185c35c56ad4989b936b
Ehan <ehanahamed@ehan.dev>
2025-09-11 09:41:46 -0400

    add better input parsing and instructions

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,22 @@ public class Main {
         while (continueLooping) {
             System.out.print("Course name: ");
             String courseName = scan.nextLine();
-            System.out.print("Type the course level: ");
+            System.out.print("Type the course level (AP, Honors) or leave blank: ");
             String courseLevel = scan.nextLine();
             CourseLevel courseLevelEnum;
             try {
-                courseLevelEnum = CourseLevel.valueOf(courseLevel);
+                courseLevelEnum = CourseLevel.valueOf(courseLevel.toUpperCase());
             } catch (IllegalArgumentException e) {
-                System.out.println("Invalid course level. Try again.");
-                continue;
+                if (courseLevel.isEmpty()) {
+                    courseLevelEnum = CourseLevel.ACADEMIC;
+                } else {
+                    System.out.println("Invalid course level. Try again.");
+                    continue;
+                }
             }
-            System.out.print("Enter your grade: ");
+            System.out.print("Enter your grade (0-100): ");
             double courseGrade = scan.nextDouble();
-            System.out.println("How many credits for this course?: ");
+            System.out.print("Credits for this course: ");
             double courseCredits = scan.nextDouble();
             classes.add(new ClassInfo(courseName, courseLevelEnum, courseGrade, courseCredits));
             scan.nextLine();
